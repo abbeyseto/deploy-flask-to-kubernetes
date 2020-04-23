2445816f37be14b5f4bd9a1103a16574a1957424
Adenle Abiodun <adenleabbey@hotmail.com>
2020-04-23 15:28:37 +0100

    Changed

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy up here 😄")
+    return jsonify("Healthy up here 😄.Yay")
 
 
 @APP.route('/auth', methods=['POST'])
